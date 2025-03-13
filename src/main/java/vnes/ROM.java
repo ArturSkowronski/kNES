18155016cf4ba09d5f27b1c86a458e81c45adfe4
Artur Skowronski <me@arturskowronski.com>
2025-03-13 22:21:58 +0100

    Repackage Mappers to new catalog

--- src/main/java/vnes/ROM.java
+++ src/main/java/vnes/ROM.java
@@ -16,8 +16,9 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import vnes.mappers.*;
+
 import java.io.*;
-import javax.swing.JOptionPane;
 
 public class ROM {
 
